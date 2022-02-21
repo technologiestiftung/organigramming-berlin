ac2c87295186a4cdeddc47e0b2a77ebecf5fdce6
Lisa-Stubert <lisastubert@web.de>
2022-02-21 17:29:19 +0100

    add InfoModal

--- app/src/components/Sidebar/Sidebar.js
+++ app/src/components/Sidebar/Sidebar.js
@@ -5,6 +5,7 @@ import DocumentTab from "./DocumentTab";
 import NewDocumentModal from "./NewDocumetModal";
 import OrganisationTab from "./OrganisationTab";
 import ExportModal from "./ExportModal";
+import InfoModal from "./InfoModal";
 import Split from "react-split";
 
 const Sidebar = ({
@@ -22,6 +23,7 @@ const Sidebar = ({
   const [activeTap, setActiveTap] = useState(null);
   const [newDocumentModalShow, setNewDocumentModalShow] = useState(false);
   const [exportModalShow, setExportModalShow] = useState(false);
+  const [infoModalShow, setInfoModalShow] = useState(false);
 
   const onChange = (e) => {
     sendDataUp(e);
@@ -57,6 +59,12 @@ const Sidebar = ({
           onExport={onExport}
           onSave={onSave}
           onHide={() => setExportModalShow(false)}
+        />
+      )}
+      {infoModalShow && (
+        <InfoModal
+          show={infoModalShow}
+          onHide={() => setInfoModalShow(false)}
         />
       )}
       <Row>
@@ -110,7 +118,7 @@ const Sidebar = ({
                     selected != null ? setActiveTap("organisation") : undefined
                   }
                   disabled={selected === null ? true : false}
-                  title="Organisations Informationen"
+                  title="Organisationsinformationen"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -132,6 +140,27 @@ const Sidebar = ({
                   setExportModalShow(true);
                 }}
                 title="Dokument speichern oder exportieren"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-download"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                </svg>
+              </Button>
+              <Button
+                variant="light"
+                className={infoModalShow ? "active" : ""}
+                onClick={() => {
+                  setActiveTap(null);
+                  setInfoModalShow(true);
+                }}
+                title="Info und Anleitung"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
