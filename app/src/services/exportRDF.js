ab765152aeb29ef12e78e579a5878162746888d9
hhack <hans.f.hack@googlemail.com>
2023-04-06 09:04:59 +0200

    working rdf export

--- app/src/services/exportRDF.js
+++ app/src/services/exportRDF.js
@@ -12,100 +12,48 @@ const downloadData = async (data, rdf) => {
   document.body.removeChild(link);
 };
 
-const context = {
-  org: "http://www.w3.org/ns/org#",
-  vcard: "http://www.w3.org/2006/vcard/ns#",
-};
-
-const member = {
-  "@type": "vcard:Individual",
-  "vcard:title": "Mr.",
-  "vcard:given-name": "Vorname",
-  "vcard:family-name": "Namchname 1",
-  "vcard:role": "Software Engineer",
-  "vcard:tel": {
-    "@type": "vcard:Work",
-    "vcard:hasValue": "tel:+1-555-555-5555",
-  },
-  "vcard:fax": {
-    "@type": "vcard:Work",
-    "vcard:hasValue": "fax:+1-555-555-5556",
-  },
-  "vcard:email": {
-    "@type": "vcard:Work",
-    "vcard:hasValue": "john.doe@example.com",
-  },
-  "vcard:url": {
-    "@type": "vcard:Work",
-    "vcard:hasValue": "https://www.example.com/johndoe",
-  },
-};
-
-const orgTemplate = {
-  "@id": "http://example.org/organizations#MyToplevelOrganization",
-  "@type": "org:Organization",
-  "org:name": "My Toplevel Organization",
-  "org:classification": "Nonprofit",
-  "org:hasSite": {
-    "@type": "org:Site",
-    "org:siteAddress": {
-      "@type": "vcard:Address",
-      "vcard:street-address": "Hausnummer + Straße + Gebäude + Raum",
-      "vcard:postal-code": "PLZ",
-      "vcard:locality": "Berlin",
-      //   "vcard:region": "Berlin",
-      //   "vcard:country-name": "Germany",
-    },
-    "vcard:tel": {
-      "@type": "vcard:Work",
-      "vcard:hasValue": "tel:TEL",
-    },
-    "vcard:fax": {
-      "@type": "vcard:Work",
-      "vcard:hasValue": "fax:FAX",
-    },
-    "vcard:email": {
-      "@type": "vcard:Work",
-      "vcard:hasValue": "john.doe@example.com",
-    },
-    "vcard:url": {
-      "@type": "vcard:Work",
-      "vcard:hasValue": "https://www.example.com/johndoe",
-    },
-  },
-  "org:hasMember": [],
-  "org:hasUnit": [],
-  "org:hasSubOrganization": [],
-};
-
 function hasKeys(obj) {
+  if (!obj) return false;
   return Object.keys(obj).length === 0 ? false : true;
 }
 
 function getMemberData(d) {
-  // {
-  //     "@type": "vcard:Individual",
-  //     "vcard:title": "Mr.",
-  //     "vcard:given-name": "Vorname",
-  //     "vcard:family-name": "Namchname 1",
-  //     "vcard:role": "Software Engineer",
-  //     "vcard:tel": {
-  //       "@type": "vcard:Work",
-  //       "vcard:hasValue": "tel:+1-555-555-5555"
-  //     },
-  //     "vcard:fax": {
-  //       "@type": "vcard:Work",
-  //       "vcard:hasValue": "fax:+1-555-555-5556"
-  //     },
-  //     "vcard:email": {
-  //       "@type": "vcard:Work",
-  //       "vcard:hasValue": "john.doe@example.com"
-  //     },
-  //     "vcard:url": {
-  //       "@type": "vcard:Work",
-  //       "vcard:hasValue": "https://www.example.com/johndoe"
-  //     }
-  //   }
+  const dC = d.contact;
+  const newMemberJSONLD = {
+    "@type": "vcard:Individual",
+    ...(d.uri && d.uri.uri && { "@id": d.uri.uri }),
+    ...(d.title && { "vcard:title": d.title }),
+    // @todo find a data property for salutation
+    ...(d.salutation && { "vcard:additional-name": d.salutation }),
+    ...(d.firstName && { "vcard:given-name": d.firstName }),
+    ...(d.lastName && { "vcard:family-name": d.lastName }),
+    ...(d.position && { "vcard:role": d.position }),
+    ...(dC.telephone && {
+      "vcard:tel": {
+        "@type": "vcard:Work",
+        "vcard:hasValue": `tel:${dC.telephone}`,
+      },
+    }),
+    ...(dC.fax && {
+      "vcard:fax": {
+        "@type": "vcard:Work",
+        "vcard:hasValue": `fax:${dC.fax}`,
+      },
+    }),
+    ...(dC.email && {
+      "vcard:email": {
+        "@type": "vcard:Work",
+        "vcard:hasValue": dC.email,
+      },
+    }),
+    ...(dC.website && {
+      "vcard:url": {
+        "@type": "vcard:Work",
+        "vcard:hasValue": dC.website,
+      },
+    }),
+  };
+  return newMemberJSONLD;
 }
 
 function getOrgData(d) {
@@ -157,11 +105,24 @@ function getOrgData(d) {
       };
     }
   }
+  if (d.employees && d.employees.length) {
+    console.log("EMPLOYYY", d.employees);
+    newOrgJSONLD["org:hasMember"] = d.employees.map((d) => {
+      return getMemberData(d);
+    });
+  }
+  if (d.departments && d.departments.length) {
+    newOrgJSONLD["org:hasUnit"] = d.departments.map((d) => {
+      console.log("ÄÄÄÄÄÄÄÄÄÄÄ", d);
+
+      return getOrgData(d);
+    });
+  }
   return newOrgJSONLD;
 }
 
 export const exportRDF = (data) => {
-  function createNestedOrganizations(inputJSON, outputObject) {
+  function createNestedOrganizations(inputJSON) {
     function traverseOrganizations(organizations) {
       if (!organizations) return;
       return organizations.map((org) => {
@@ -176,179 +137,18 @@ export const exportRDF = (data) => {
       });
     }
 
-    outputObject.yyy = traverseOrganizations(inputJSON);
+    return traverseOrganizations(inputJSON)[0];
   }
 
-  const outputObject = {};
-  createNestedOrganizations(data.organisations, outputObject);
+  const orgs = createNestedOrganizations(data.organisations);
+  let rdf = {
+    "@context": {
+      org: "http://www.w3.org/ns/org#",
+      vcard: "http://www.w3.org/2006/vcard/ns#",
+    },
+    ...orgs,
+  };
 
-  console.log(outputObject);
+  console.log(rdf);
+  downloadData(data, rdf);
 };
-
-// [
-//   {
-//     id: "n3",
-//     name: "Oberste Organisationseinheit",
-//     organisations: [
-//       {
-//         uri: {},
-//         style: "default",
-//         background: {
-//           color: "",
-//           style: "default",
-//         },
-//         address: {},
-//         contact: {},
-//         employees: [
-//           {
-//             uri: {},
-//             contact: {
-//               telephone: "030 343566",
-//             },
-//             firstName: "Max",
-//             lastName: "Muster",
-//             position: "Abteilungsleiter",
-//           },
-//           {
-//             uri: {},
-//             contact: {
-//               telephone: "030 565767",
-//             },
-//             firstName: "Miriam",
-//             lastName: "Muster",
-//             position: "Stellvertretende Abteilungsleiterin",
-//           },
-//         ],
-//         departments: [
-//           {
-//             name: "Sekretariat",
-//             employees: [
-//               {
-//                 uri: {},
-//                 contact: {
-//                   telephone: "030 3568355",
-//                 },
-//                 firstName: "Marie",
-//                 lastName: "Tester",
-//               },
-//               {
-//                 uri: {},
-//                 contact: {
-//                   telephone: "030 3457689",
-//                 },
-//                 firstName: "Tim",
-//                 lastName: "Tester",
-//               },
-//             ],
-//           },
-//         ],
-//         organisations: [],
-//         suborganizationOrientation: "horizontal",
-//         id: "n5",
-//         name: "Organisationsuntereinheit",
-//         type: "Abteilung",
-//       },
-//       {
-//         uri: {},
-//         style: "default",
-//         background: {
-//           color: "",
-//           style: "default",
-//         },
-//         address: {},
-//         contact: {},
-//         suborganizationOrientation: "horizontal",
-//         type: "Neue",
-//         name: "Organisationsuntereinheit",
-//         id: "n6",
-//       },
-//       {
-//         uri: {},
-//         style: "default",
-//         background: {
-//           color: "",
-//           style: "default",
-//         },
-//         address: {},
-//         contact: {},
-//         organisations: [
-//           {
-//             uri: {},
-//             style: "light",
-//             background: {
-//               color: "",
-//               style: "default",
-//             },
-//             address: {},
-//             contact: {},
-//             organisations: [],
-//             suborganizationOrientation: "horizontal",
-//             id: "n9",
-//             name: "Nachgeordnete Organisationseinheit",
-//           },
-//         ],
-//         suborganizationOrientation: "horizontal",
-//         id: "n4",
-//         name: "Organisationsuntereinheit",
-//       },
-//     ],
-//     style: "emphasized",
-//     contact: {
-//       website: "www.berlin.de",
-//     },
-//     address: {
-//       street: "Unter den Linden",
-//       housenumber: "1",
-//       building: "AB",
-//       room: "100",
-//       zipCode: "12345",
-//       city: "Berlin",
-//     },
-//     departments: [
-//       {
-//         name: "Neue Organisationsuntereinheit",
-//         employees: [
-//           {
-//             uri: {
-//               uri: "http://www.wikidata.org/entity/Q650",
-//               uriLabel: "Fluor",
-//               uriDescription:
-//                 "chemisches Element mit dem Symbol F und der Ordnungszahl 9",
-//             },
-//             contact: {},
-//           },
-//         ],
-//       },
-//     ],
-//     type: "Bezirksamt",
-//     employees: [
-//       {
-//         uri: {
-//           uri: "http://www.wikidata.org/entity/Q573",
-//           uriLabel: "Tag",
-//           uriDescription:
-//             "Einheit für einen Zeitraum mit verschiedenen Bedeutungen",
-//         },
-//         contact: {
-//           email: "muster@berlin.de",
-//           telephone: "030 345678",
-//           fax: "030 345679",
-//         },
-//         title: "Dr.",
-//         firstName: "Erika",
-//         lastName: "Muster",
-//       },
-//     ],
-//     uri: {
-//       uri: "http://www.wikidata.org/entity/Q623",
-//       uriLabel: "Kohlenstoff",
-//       uriDescription:
-//         "chemisches Element mit dem Symbol C und der Ordnungszahl 6",
-//     },
-//     background: {
-//       color: "",
-//       style: "default",
-//     },
-//     suborganizationOrientation: "horizontal",
-//   },
-// ];
