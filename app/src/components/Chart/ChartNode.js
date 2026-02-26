167aa3c15baa0b7d7dd0ed1510fd4300d18a08c1
hhack <hans.f.hack@googlemail.com>
2026-02-26 14:35:59 +0100

    fix: prevent address null access

--- app/src/components/Chart/ChartNode.js
+++ app/src/components/Chart/ChartNode.js
@@ -367,7 +367,7 @@ const ChartNode = forwardRef(
                   </ul>
                 )}
 
-                {(ds?.address?.street || ds.address.city) ? (
+                {(ds?.address?.street || ds.address?.city) ? (
                   <div className="address">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -379,16 +379,16 @@ const ChartNode = forwardRef(
                     >
                       <path d="M7 1.414V4H2a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h5v6h2v-6h3.532a1 1 0 0 0 .768-.36l1.933-2.32a.5.5 0 0 0 0-.64L13.3 4.36a1 1 0 0 0-.768-.36H9V1.414a1 1 0 0 0-2 0zM12.532 5l1.666 2-1.666 2H2V5h10.532z" />
                     </svg>
-                    {ds.address.street} {ds.address.housenumber && " "}
-                    {ds.address.housenumber}
-                    {ds.address.building && " "}
-                    {ds.address.building}
-                    {ds.address.room && "-"}
-                    {ds.address.room}
-                    {ds.address.zipCode && ", "}
-                    {ds.address.zipCode}
-                    {ds.address.city && " "}
-                    {ds.address.city}
+                    {ds?.address?.street} {ds?.address?.housenumber && " "}
+                    {ds?.address?.housenumber}
+                    {ds?.address?.building && " "}
+                    {ds?.address?.building}
+                    {ds?.address?.room && "-"}
+                    {ds?.address?.room}
+                    {ds?.address?.zipCode && ", "}
+                    {ds?.address?.zipCode}
+                    {ds?.address?.city && " "}
+                    {ds?.address?.city}
                   </div>
                 ):""}
               </div>
